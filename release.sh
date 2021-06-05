af7217719e83d4d17ff7ebbb31ffee8a736b20f5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-06-05 10:14:51 +0200

    udpate

--- release.sh
+++ release.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 rm -rf release
 mkdir -p release
-cp -rf NDI *.{hpp,cpp,json} CMakeLists.txt release/
+cp -rf NDI *.{hpp,cpp,txt} release/
+
+VERSION=$(git tag)
+cat addon.json | sed  "s/\$VERSION/$VERSION/g" > release/addon.json
+
 mkdir -p release/3rdparty
 cp -rf 3rdparty/NDI release/3rdparty
 
