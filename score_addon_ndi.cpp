59a82092c867bcca558b55c14d558167fcfb82ed
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-11-11 14:44:17 -0500

    [ndi] Disable NDI warning

--- score_addon_ndi.cpp
+++ score_addon_ndi.cpp
@@ -1,16 +1,18 @@
 #include "score_addon_ndi.hpp"
 
 #include <score/plugins/FactorySetup.hpp>
-#include <Ndi/InputFactory.hpp>
-#include <Ndi/OutputFactory.hpp>
-#include <Ndi/Loader.hpp>
 #include <score/widgets/MessageBox.hpp>
+
 #include <QTimer>
+
+#include <Ndi/InputFactory.hpp>
+#include <Ndi/Loader.hpp>
+#include <Ndi/OutputFactory.hpp>
 
 score_addon_ndi::score_addon_ndi()
 {
   m_hasNDI = Ndi::Loader::instance().available();
-
+  /*
   if(!m_hasNDI)
   {
     QTimer::singleShot(1, [] {
@@ -20,14 +22,12 @@ score_addon_ndi::score_addon_ndi()
                    "NDI is searched for in the NDI_RUNTIME_DIR_V4 environment variable by default.");
     });
   }
+  */
 }
 
-score_addon_ndi::~score_addon_ndi()
-{
-}
+score_addon_ndi::~score_addon_ndi() { }
 
-std::vector<std::unique_ptr<score::InterfaceBase>>
-score_addon_ndi::factories(
+std::vector<std::unique_ptr<score::InterfaceBase>> score_addon_ndi::factories(
     const score::ApplicationContext& ctx, const score::InterfaceKey& key) const
 {
   if(!m_hasNDI)
@@ -35,13 +35,8 @@ score_addon_ndi::factories(
 
   return instantiate_factories<
       score::ApplicationContext,
-      FW<Device::ProtocolFactory
-      , Ndi::InputFactory
-      , Ndi::OutputFactory
-      >
-  >(ctx, key);
+      FW<Device::ProtocolFactory, Ndi::InputFactory, Ndi::OutputFactory>>(ctx, key);
 }
-
 
 #include <score/plugins/PluginInstances.hpp>
 SCORE_EXPORT_PLUGIN(score_addon_ndi)
